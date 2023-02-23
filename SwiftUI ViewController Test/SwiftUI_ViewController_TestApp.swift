d77b4abd6bb54ac05bf92e4135dc5582437558dd
Jose Vazquez <jose@teguz.com>
2023-02-23 13:52:00 -0600

    Initial Commit

---
+++ SwiftUI ViewController Test/SwiftUI_ViewController_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ViewController_TestApp.swift
+//  SwiftUI ViewController Test
+//
+//  Created by Jose Vazquez on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ViewController_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
